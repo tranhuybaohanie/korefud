54927f2dbdbaa18cd00703358f9c8de7a4acb4a5
tranhuybaohanie <tranhuybaohanie@gmail.com>
2018-07-21 08:34:11 +0700

    mock

---
+++ Mock/FoodList.js
@@ -0,0 +1,16 @@
+export default FoodList=[
+    {
+        id:1,
+        nameFood: "Gà hấp xã chiên nước mắm luộc",
+        imgUrl:"https://cdn.cnn.com/cnnnext/dam/assets/171027052520-processed-foods-exlarge-tease.jpg",
+        price:"200000",
+        pricepromotion:"100000",
+        description:"thịt gà ngon tuyệt lun nè ăn thử là ghiền nha :)))"
+    },
+    {
+        id:2,
+        nameFood: "Chicken",
+        imgUrl:"https://cdn.cnn.com/cnnnext/dam/assets/171027052520-processed-foods-exlarge-tease.jpg"
+    },
+    
+]
